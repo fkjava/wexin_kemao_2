54d86d9c8d2426959c1624b4a59323bf283a3be8
Luo Wenqiang <luo_wenqiang@126.com>
2019-03-20 20:25:40 +0800

    接收微信公众号平台发送给我们的消息

--- src/main/java/org/fkjava/wexin/controller/MessageReceiverController.java
+++ src/main/java/org/fkjava/wexin/controller/MessageReceiverController.java
@@ -1,6 +1,10 @@
 package org.fkjava.wexin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/kemao_2/message/receiver")
 public class MessageReceiverController {
 
+	// 日志记录器
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
+
 	// 注意：控制器里面必须要有处理器方法（Handler Method）才能执行操作，才不会404
 	// 处理GET请求，HTTP协议支持GET、POST、PUT、DELETE等请求方式，都有对应的注解
 	@GetMapping
@@ -25,5 +32,24 @@ public class MessageReceiverController {
 	//
 	) {
 		return echostr;
+	}
+
+	@PostMapping
+	// @RequestBody注解表示把请求内容获取出来，并且转换为String传入给xml参数。
+	public String onMessage(//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestBody String xml) {
+		// 收到消息
+		// {}是占位符，第一个{}会把第二个参数的值自动填入
+		// LOG.trace必须要求日志记录器的配置为trace级别才能输出
+		LOG.trace("收到的消息原文：\n{}\n------------------------------", xml);
+		// 转换消息
+		// 把消息丢入队列
+		// 消费队列中的消息
+		// 产生客服消息
+
+		return "success";
 	}
 }
